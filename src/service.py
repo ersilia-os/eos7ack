ec9de37d53b61a113979b7e800c413572c98695e
Amna Ali <98670206+Amna-28@users.noreply.github.com>
2022-10-12 01:25:20 +0200

    Update service.py

--- src/service.py
+++ src/service.py
@@ -16,17 +16,20 @@ import csv
 CHECKPOINTS_BASEDIR = "checkpoints"
 FRAMEWORK_BASEDIR = "framework"
 
+
 def load_model(framework_dir, checkpoints_dir):
     mdl = Model()
     mdl.load(framework_dir, checkpoints_dir)
     return mdl
+
 
 def Float(x):
     try:
         return float(x)
     except:
         return None
-    
+
+
 def String(x):
     x = str(x)
     if not x:
@@ -41,11 +44,13 @@ def String(x):
         return None
     return x
 
+
 def Mixed(x):
     try:
         return float(x)
     except:
         return String(x)
+
 
 class Model(object):
     def __init__(self):
@@ -64,22 +69,22 @@ class Model(object):
     def set_framework_dir(self, dest):
         self.framework_dir = os.path.abspath(dest)
 
-    def predict(self, smiles_list): # <-- EDIT: rename if model does not do predictions (e.g. it does calculations)
+    def predict(
+        self, smiles_list
+    ):  # <-- EDIT: rename if model does not do predictions (e.g. it does calculations)
         tmp_folder = tempfile.mkdtemp(prefix="eos-")
         data_file = os.path.join(tmp_folder, self.DATA_FILE)
         output_file = os.path.join(tmp_folder, self.OUTPUT_FILE)
         log_file = os.path.join(tmp_folder, self.LOG_FILE)
         with open(data_file, "w") as f:
-            f.write("smiles"+os.linesep)
+            f.write("smiles" + os.linesep)
             for smiles in smiles_list:
-                f.write(smiles+os.linesep)
+                f.write(smiles + os.linesep)
         run_file = os.path.join(tmp_folder, self.RUN_FILE)
         with open(run_file, "w") as f:
             lines = [
-                "bash {0}/run_predict.sh {0} {1} {2}".format( # <-- EDIT: match method name (run_predict.sh, run_calculate.sh, etc.)
-                    self.framework_dir,
-                    data_file,
-                    output_file
+                "bash {0}/run_predict.sh {0} {1} {2}".format(  # <-- EDIT: match method name (run_predict.sh, run_calculate.sh, etc.)
+                    self.framework_dir, data_file, output_file
                 )
             ]
             f.write(os.linesep.join(lines))
@@ -101,7 +106,7 @@ class Model(object):
         shutil.rmtree(tmp_folder)
         return result
 
-    
+
 class Artifact(BentoServiceArtifact):
     def __init__(self, name):
         super(Artifact, self).__init__(name)
@@ -152,8 +157,10 @@ class Artifact(BentoServiceArtifact):
 @artifacts([Artifact("model")])
 class Service(BentoService):
     @api(input=JsonInput(), batch=True)
-    def predict(self, input: List[JsonSerializable]): # <-- EDIT: rename if necessary 
+    def predict(self, input: List[JsonSerializable]):  # <-- EDIT: rename if necessary
         input = input[0]
         smiles_list = [inp["input"] for inp in input]
-        output = self.artifacts.model.predict(smiles_list) # <-- EDIT: rename if necessary
+        output = self.artifacts.model.predict(
+            smiles_list
+        )  # <-- EDIT: rename if necessary
         return [output]
